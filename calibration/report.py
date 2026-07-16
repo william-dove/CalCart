2eb3bc8fb4e20dd0995aad03e7082983975deb80
William Dove <William.Dove@ima.it>
2026-07-16 08:16:57 -0400

    removed report.py

--- calibration/report.py
+++
@@ -1,8 +0,0 @@
-#calibration/report.py
-import xlwings as xw
-
-def generate_report(results, resultspath):
-    '''
-    Makes a PDF report after running a calibration sequence from the GUI.
-    '''
-    
